492667e4bc98924dd43724e79eeebca7c4465ce6
Abobakreid <bkryd849@gmail.com>
2025-02-06 18:21:25 +0200

    update middleware

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -44,8 +44,7 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !pathname.startsWith("/login") &&
     !pathname.startsWith("/signup") &&
-    !pathname.startsWith("/admin") &&
-    pathname === ""
+    !pathname.startsWith("/admin")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
